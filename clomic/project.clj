882c05b9f0e43018a5470bce0d5a51624cfd5b48
SergeJohanns <serge.johanns@gmail.com>
2020-09-20 17:55:20 +0200

    Add leiningen project template files
    Add the default files for clojure development with stub/placeholder content, as
    well as a .gitignore suited for files automatically generated by leiningen.
    
    Note that the license, which is automatically added by leiningen, has been
    removed as a standalone file and from project.clj. A license will be added
    later, but it may not be the EPL, as added by leiningen.

---
+++ clomic/project.clj
@@ -0,0 +1,4 @@
+(defproject clomic "0.1.0-SNAPSHOT"
+  :description "A telegram bot that automatically sends you new issues of webcomics you follow."
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns clomic.core})
