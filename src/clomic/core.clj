c33b3bebd6a8680777e471c4a2554c887d1e8bfc
SergeJohanns <serge.johanns@gmail.com>
2020-09-22 18:39:17 +0200

    Fix style guide violations
    Change an instance of contains? on a set and an element to applying the set
    itself as a predicate, and change two instances of if do with when instead.

--- src/clomic/core.clj
+++ src/clomic/core.clj
@@ -7,22 +7,20 @@
   (contains? feeds feed))
 
 (defn subscribed? [user-id feed]
-  (and (feed? feed) (contains? (@subscriptions feed) user-id)))
+  (and (feed? feed) ((@subscriptions feed) user-id)))
 
 (defn update-subscriptions [feed f]
   (let [update-feed (fn [x g] (update x feed g))]
     (dosync (alter subscriptions update-feed f))))
 
 (defn subscribe [user-id feed]
-  (if (feed? feed)
-    (do (if (contains? @subscriptions feed)
-          (update-subscriptions feed (fn [y] (conj y user-id)))
-          (update-subscriptions feed (fn [_] #{user-id})))
-        user-id)
-    nil))
+  (when (feed? feed)
+    (if (contains? @subscriptions feed)
+      (update-subscriptions feed (fn [y] (conj y user-id)))
+      (update-subscriptions feed (fn [_] #{user-id})))
+    user-id))
 
 (defn unsubscribe [user-id feed]
-  (if (subscribed? user-id feed)
-    (do (update-subscriptions feed (fn [y] (disj y user-id)))
-        user-id)
-    nil))
+  (when (subscribed? user-id feed)
+    (update-subscriptions feed (fn [y] (disj y user-id)))
+    user-id))
