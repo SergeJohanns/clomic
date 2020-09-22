f46f2de9ba9c29dd6cb2b6d2e23fd0c634597466
SergeJohanns <serge.johanns@gmail.com>
2020-09-22 05:25:14 +0200

    Add basic subscription tracking

--- src/clomic/core.clj
+++ src/clomic/core.clj
@@ -1,6 +1,28 @@
 (ns clomic.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def subscriptions (ref {}))
+(def feeds (ref {}))
+
+(defn feed? [feed]
+  (contains? @feeds feed))
+
+(defn subscribed? [user-id feed]
+  (and (feed? feed) (contains? (@subscriptions feed) user-id)))
+
+(defn update-subscriptions [feed f]
+  (let [update-feed (fn [x g] (update x feed g))]
+    (dosync (alter subscriptions update-feed f))))
+
+(defn subscribe [user-id feed]
+  (if (feed? feed)
+    (do (if (contains? @subscriptions feed)
+          (update-subscriptions feed (fn [y] (conj y user-id)))
+          (update-subscriptions feed (fn [_] #{user-id})))
+        user-id)
+    nil))
+
+(defn unsubscribe [user-id feed]
+  (if (subscribed? user-id feed)
+    (do (update-subscriptions feed (fn [y] (disj y user-id)))
+        user-id)
+    nil))
--- test/clomic/core_test.clj
+++ test/clomic/core_test.clj
@@ -1,3 +1,29 @@
 (ns clomic.core-test
   (:require [clojure.test :refer :all]
             [clomic.core :refer :all]))
+
+(defn set-mock-feeds [f]
+  (dosync (ref-set feeds {"xkcd" nil}))
+  (f)
+  (dosync (ref-set feeds {})))
+
+(use-fixtures :each set-mock-feeds)
+
+(deftest test-subscribe-user
+  (testing "Subscribe a user to an existing feed"
+    (is (= 0 (subscribe 0 "xkcd")))
+    (is (subscribed? 0 "xkcd")))
+  (testing "Fail to subscribe a user to a non-existing feed"
+    (is (= nil (subscribe 0 "this-is-not-a-real-feed")))))
+
+(deftest test-unsubscribe-user
+  (subscribe 42 "xkcd")
+  (testing "Remove a subscribed user from an existing feed"
+    (is (subscribed? 42 "xkcd"))
+    (is (= 42 (unsubscribe 42 "xkcd")))
+    (is (not (subscribed? 42 "xkcd"))))
+  (testing "Fail to remove an unsubscribed user from an existing feed"
+    (is (not (subscribed? 41 "xkcd")))
+    (is (= nil (unsubscribe 41 "xkcd"))))
+  (testing "Fail to remove a user from a non-existing feed"
+    (is (= nil (unsubscribe 42 "this-is-not-a-real-feed")))))
