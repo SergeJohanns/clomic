1c84400934a4082f552cce54d61cbe86893b8f69
SergeJohanns <serge.johanns@gmail.com>
2020-09-22 17:45:23 +0200

    Eliminate magic numbers in tests

--- test/clomic/core_test.clj
+++ test/clomic/core_test.clj
@@ -2,28 +2,37 @@
   (:require [clojure.test :refer :all]
             [clomic.core :refer :all]))
 
+(def user 42)
+(def subscribed-user 43)
+(def unsubscribed-user 44)
+(def real-feed "xkcd")
+(def fake-feed "dckx")
+
 (defn set-mock-feeds [f]
   (dosync (ref-set feeds {"xkcd" nil}))
   (f)
   (dosync (ref-set feeds {})))
 
-(use-fixtures :each set-mock-feeds)
+(defn set-mock-users [f]
+  (dosync (ref-set subscriptions {real-feed #{subscribed-user}}))
+  (f))
+
+(use-fixtures :each set-mock-feeds set-mock-users)
 
 (deftest test-subscribe-user
   (testing "Subscribe a user to an existing feed"
-    (is (= 0 (subscribe 0 "xkcd")))
-    (is (subscribed? 0 "xkcd")))
+    (is (= user (subscribe user real-feed)))
+    (is (subscribed? user real-feed)))
   (testing "Fail to subscribe a user to a non-existing feed"
-    (is (= nil (subscribe 0 "this-is-not-a-real-feed")))))
+    (is (= nil (subscribe user fake-feed)))))
 
 (deftest test-unsubscribe-user
-  (subscribe 42 "xkcd")
   (testing "Remove a subscribed user from an existing feed"
-    (is (subscribed? 42 "xkcd"))
-    (is (= 42 (unsubscribe 42 "xkcd")))
-    (is (not (subscribed? 42 "xkcd"))))
+    (is (subscribed? subscribed-user real-feed))
+    (is (= subscribed-user (unsubscribe subscribed-user real-feed)))
+    (is (not (subscribed? subscribed-user real-feed))))
   (testing "Fail to remove an unsubscribed user from an existing feed"
-    (is (not (subscribed? 41 "xkcd")))
-    (is (= nil (unsubscribe 41 "xkcd"))))
+    (is (not (subscribed? unsubscribed-user real-feed)))
+    (is (= nil (unsubscribe unsubscribed-user real-feed))))
   (testing "Fail to remove a user from a non-existing feed"
-    (is (= nil (unsubscribe 42 "this-is-not-a-real-feed")))))
+    (is (= nil (unsubscribe user fake-feed)))))
